57aa71af0c46475e6962c3b0acad0d6574ae4225
Irina Kvitkina <irina.kvitkina@gmail.com>
2020-10-13 14:51:59 +0300

    eslint

--- routes/cards.js
+++ routes/cards.js
@@ -1,11 +1,12 @@
-const router = require('express').Router()
-const path = require ('path')
-const readFile = require('../utils/read-file.js')
-const jsonCardsPath = path.join(__dirname, '..', 'data', 'cards.json')
+const router = require('express').Router();
+const path = require('path');
+const readFile = require('../utils/read-file.js');
 
-router.get ('/cards', (req, res) => {
+const jsonCardsPath = path.join(__dirname, '..', 'data', 'cards.json');
+
+router.get('/cards', (req, res) => {
   readFile(jsonCardsPath)
-  .then (data => res.send(data))
-  .catch (err => res.send({massage: "Ошибка чтения файла"}))
-})
-module.exports = router
+    .then((data) => res.send(data))
+    .catch(() => res.status(500).send({ massage: 'Ошибка чтения файла' }));
+});
+module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,27 +1,29 @@
-const router = require('express').Router()
-const path = require ('path')
-const readFile = require('../utils/read-file.js')
-const jsonUsersPath = path.join(__dirname, '..', 'data', 'users.json')
+const router = require('express').Router();
+const path = require('path');
+const readFile = require('../utils/read-file');
 
-router.get ('/users', (req, res) => {
-  readFile(jsonUsersPath)
-  .then (data => res.send(data))
-  .catch (err => res.send({massage: "Ошибка чтения файла"}))
-})
+const jsonUsersPath = path.join(__dirname, '..', 'data', 'users.json');
 
-router.get('/users/:id', (req,res) => {
-  const { id } = req.params
+router.get('/users', (req, res) => {
   readFile(jsonUsersPath)
-  .then (data => {
-    const userToFind = data.find(user => user._id === id)
-    return userToFind
-  })
-  .then (user => {
-    if(!user) {
-      return res.status(404).send({message: "Нет пользователя с таким id"})
-    }
-    res.send(user)
-  })
-  .catch (err => res.send({massage: "Ошибка чтения файла"}))
-})
-module.exports = router
+    .then((data) => res.send(data))
+    .catch(() => res.status(500).send({ massage: 'Ошибка чтения файла' }));
+});
+
+router.get('/users/:id', (req, res) => {
+  const { id } = req.params;
+  readFile(jsonUsersPath)
+    .then((data) => {
+      const userToFind = data.find((user) => user._id === id);
+      return userToFind;
+    })
+    // eslint-disable-next-line consistent-return
+    .then((user) => {
+      if (!user) {
+        return res.status(404).send({ message: 'Нет пользователя с таким id' });
+      }
+      res.send(user);
+    })
+    .catch(() => res.status(500).send({ massage: 'Ошибка чтения файла' }));
+});
+module.exports = router;
